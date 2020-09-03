0592da297fdafdccf290d7bd95fe8c814ce89dad
Martin Nobis <martin.nobis@gmail.com>
2020-09-03 16:12:03 +1000

    Updates

---
+++ precache-manifest.2fb56457633fc07bc29578fb7f261cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c021c9df815ff9552fdde4a56cc0dad3",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "9d10842023703e596834",
+    "url": "/react-test/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f712a082fd4d4ca10b9f",
+    "url": "/react-test/static/css/main.97162626.chunk.css"
+  },
+  {
+    "revision": "9d10842023703e596834",
+    "url": "/react-test/static/js/2.d45e2711.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-test/static/js/2.d45e2711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f712a082fd4d4ca10b9f",
+    "url": "/react-test/static/js/main.4bfb51dd.chunk.js"
+  },
+  {
+    "revision": "2d4e99fa96d65cb718e2",
+    "url": "/react-test/static/js/runtime-main.2d049988.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/martinnobis/react-test/precache-manifest.b5fbaa1defe264c34a6860f2037694ca.js"
+  "/react-test/precache-manifest.2fb56457633fc07bc29578fb7f261cf9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/martinnobis/react-test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
